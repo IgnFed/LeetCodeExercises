61570b681e880af89f2e9890ddd3718b166e78aa
Ignacio Fedorenco <70576040+IgnFed@users.noreply.github.com>
2022-03-13 07:10:37 -0300

    Update 6-zigzag-convetions.js

--- 6-zigzag-convetions.js
+++ 6-zigzag-convetions.js
@@ -1,22 +1,23 @@
-function convert(str, rows) {
-  const cantCols = Math.floor(str.length / rows);
-  const arr = [[]];
-  let idxRow = 0, idxCol = 0, strIdx = 0;
-  console.log(cantCols)
-  while (idxCol != cantCols && idxRow != rows) {
-
-    if (idxCol > cantCols) {
-      idxCol = 0;
-      idxRow++;
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  if (s.length <= numRows || numRows < 2) return s;
+  var len = s.length;
+  var num = 2 * (numRows - 1);
+  var res = Array(numRows).fill('');
+  var tmp = 0;
+  for (var i = 0; i < len; i++) {
+    tmp = i % num;
+    if (tmp < numRows) {
+      res[tmp] += s[i];
+    } else {
+      res[num - tmp] += s[i];
     }
-
-    arr[idxRow][idxCol] = str[strIdx];
-    idxCol++;
-    strIdx++;
   }
+  return res.join('');
+};
 
-  return arr;
-}
-
-
-console.log(convert("PAYPALISHIRING", 4))
+convert("PAYPALISHIRING", 4)
